4a6f1bacd496c8275985b566f182e2e50e7d0878
13tom13 <13tom13@mail.ru>
2023-08-01 08:21:54 +0200

    fixed minor bugs

--- src/main/java/com/netology/springhibernate/repository/SpringHibernateRepository.java
+++ src/main/java/com/netology/springhibernate/repository/SpringHibernateRepository.java
@@ -1,8 +1,9 @@
 package com.netology.springhibernate.repository;
 
-import com.netology.springhibernate.model.Person;
-import jakarta.persistence.*;
-import jakarta.transaction.Transactional;
+import com.netology.springhibernate.entity.Person;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,11 +11,14 @@ import java.util.List;
 @Repository
 public class SpringHibernateRepository {
     @PersistenceContext
-    EntityManager entityManager;
+    private EntityManager entityManager;
 
-    @Transactional
+
     public List getPersonsByCity(String city) {
-        Query query = entityManager.createQuery("select person from Person person  where person.city_of_living = :city", Person.class);
+        Query query = entityManager
+                .createQuery(
+                        "select person from Person person  " +
+                                "where person.cityOfLiving = :city", Person.class);
         query.setParameter("city", city);
         return query.getResultList();
     }
