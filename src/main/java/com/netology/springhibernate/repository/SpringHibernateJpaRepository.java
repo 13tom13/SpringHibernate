280b752d68a3091ccd21ecf175352d65142aa16a
13tom13 <13tom13@mail.ru>
2023-08-03 16:38:05 +0200

    implementation through @Query

--- src/main/java/com/netology/springhibernate/repository/SpringHibernateJpaRepository.java
+++ src/main/java/com/netology/springhibernate/repository/SpringHibernateJpaRepository.java
@@ -3,6 +3,8 @@ package com.netology.springhibernate.repository;
 import com.netology.springhibernate.entity.Person;
 import com.netology.springhibernate.entity.PersonIDClass;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,9 +13,15 @@ import java.util.Optional;
 @Repository
 public interface SpringHibernateJpaRepository extends JpaRepository<Person, PersonIDClass> {
 
-    List<Person> findByCityOfLiving_Name(String name);
 
-    List<Person> findByAgeIsLessThanOrderByAgeAsc(int age);
+    @Query("select p from Person p where p.cityOfLiving.name = :name")
+    List<Person> findByCityOfLiving_Name(@Param("name") String name);
 
+    @Query("select p from Person p where p.age < :age order by p.age asc")
+    List<Person> findByAgeIsLessThanOrderByAgeAsc(@Param("age") int age);
+
+    @Query(value = "SELECT * from persons p where p.name = ?1 and p.surname = ?2", nativeQuery = true)
     Optional<Person> findByNameAndSurname(String name, String surname);
+
+
 }
